71ee19234decd5e1c3cc5a55842987efead3570d
lapma12 <112930985+lapma12@users.noreply.github.com>
2022-10-04 13:27:41 +0200

    Add files via upload

---
+++ Házifeladat.py
@@ -0,0 +1,8 @@
+#Házi feladat
+szam1 = input('Kérem az első számot 1-100 között: ')
+szam2 = input('Kérem a második számot 1-100 között: ')
+szam1 = int(szam1)
+szam2 = int(szam2)
+print('A két szám összege:',szam1 + szam2)
+print('A két szám külünbsége:',szam1 - szam2)
+print('A két szám szorzata:',szam1 * szam2)
