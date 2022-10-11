2303320ae85eec7668e617b15389d2106618d3bb
lapma12 <112930985+lapma12@users.noreply.github.com>
2022-10-11 09:39:22 +0200

    Add files via upload

---
+++ 11-12ora.py
@@ -0,0 +1,30 @@
+#Feladat1 
+"""
+gondolt_szam = 4
+tipp = input('Gondoltam egy számra.Tippeld meg!')
+tipp = int(tipp)
+if tipp == gondolt_szam:
+ print('Ez az igaz ág')
+ print('Ügyes!')
+
+else:
+    print('Hosszan gondolkotál rajta?')
+    print('Nem érte meg.;')
+print('pápá')
+"""
+#Feladat2
+szam1 = input('Kérek egy számot: ')
+szam1 = int(szam1)
+szam2 = input('Kérek egy másik számot: ')
+szam2 = int(szam2)
+if szam1 == szam2:
+    print('A két szám megegyezik')
+if szam1 != szam2:
+    print('A két szám nem egyezik meg')
+print('A program vége')
+if szam1 < szam2:
+    print('Szam1 kisebb mint a szam2')
+else:
+    print('Az első szám nagyobb vagy egyenlő')
+    
+
