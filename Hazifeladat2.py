e5c34cec9fd768b890c28fdb259a9f2fa0dd3b1a
lapma12 <112930985+lapma12@users.noreply.github.com>
2022-10-17 14:33:08 +0200

    Update Hazifeladat2.py

--- Hazifeladat2.py
+++ Hazifeladat2.py
@@ -1,3 +1,4 @@
+#Házi feladat2
 szam1 = int(input('Kérem az első számot 1-100 között: '))
 szam2 = int(input('Kérem a második számot 1-100 között: '))
 if szam1 == szam2:
@@ -7,4 +8,4 @@ if szam1 > szam2:
     print('A második szám kisebb, a különbségük:',szam1 - szam2) 
 if szam1 < szam2:
     print('két szám nem egyezik meg ')
-    print('A második szám kisebb, a különbségük:',szam1 - szam2) 
+    print('A második szám nagyobb, a különbségük:',szam1 - szam2) 
