56409158bc775552af1099e6487e1eae49603a43
lapma12 <112930985+lapma12@users.noreply.github.com>
2022-11-15 09:41:11 +0100

    Add files via upload

---
+++ 20orahazi.py
@@ -0,0 +1,34 @@
+#2.feladat
+"""
+szam = 1
+while szam < 100:
+    szam += 2
+    print(szam)
+    
+print("Vége")
+"""
+#3.feladat
+"""
+szam = 0
+while szam < 100:
+    szam += 3
+    if szam <= 100:
+        print(szam)
+print("Vége")"""
+
+#4.feladat
+"""szam = 0
+while szam < 100:
+    if szam % 2 == 0:
+        print(szam)
+    szam += 2
+print("Vége")"""
+
+"""
+#5.feladat
+szam = 1
+while szam < 100:
+    if szam % 2 == 1:
+        print(szam)
+    szam += 2
+print("Vége")"""
