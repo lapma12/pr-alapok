39e220c80fa22825fbfe4056acbca9990c99d8aa
lapma12 <112930985+lapma12@users.noreply.github.com>
2022-11-24 20:21:22 +0100

    Add files via upload

---
+++ 22hazi.py
@@ -0,0 +1,36 @@
+"""
+2.
+for _ in range(1,101):
+    print(_)
+"""
+
+"""
+3.
+for index in range(1,101,3):
+    print(index)
+"""   
+
+
+"""
+4.
+for _ in range(101):
+    if _ % 2 == 0:
+        print(_)
+"""
+
+"""
+5.
+for _ in range(101):
+    if _ % 2 == 1:
+        print(_)
+"""
+
+"""
+6.
+s = "Házi feladat"
+hossz = len(s)
+szamlalo = 0
+for szamlalo in range(hossz):
+    print(s[szamlalo])
+    szamlalo = szamlalo + 1
+"""
