dcad685c25d9846595daef29c04e471ad9795df3
lapma12 <112930985+lapma12@users.noreply.github.com>
2023-02-15 07:56:27 +0100

    Add files via upload

---
+++ feladat_2.py
@@ -0,0 +1,11 @@
+#Lapostyán Martin 10.C, 2.CSOPORT
+while True:
+    szo = input("Adj meg szavakat: ")
+    print(f"{szo}") 
+    if szo == "":
+        print("Program vége!")
+        break
+print(f"{szo}") 
+
+
+
