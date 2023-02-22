2468963da0ff4d7e690bf7897361ff393f0d0932
lapma12 <112930985+lapma12@users.noreply.github.com>
2023-02-22 07:38:20 +0100

    Add files via upload

---
+++ feladat_3.py
@@ -0,0 +1,9 @@
+#Lapostyán Martin 10.c 2.csoport" 3.Feladat
+
+honapok = ["január","február","március","április","május","június"]
+
+index = 0
+for honap in honapok:
+    print(index,honap)
+    index += 1
+print("Progam Vége!")
